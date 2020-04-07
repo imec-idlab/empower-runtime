68ce770451d213b899cabb8e97b73ece54ae2036
Pedro Heleno Isolani <pedroisolani@gmail.com>
2020-04-07 17:35:23 +0200

    Init script updated

--- init_controller.sh
+++ init_controller.sh
@@ -5,12 +5,6 @@ MINIMUM_QUANTUM=5
 QUANTUM_DECREASE_RATE=0.3
 QUANTUM_INCREASE_RATE=0.1
 
-# Flow descriptor filename,
-# the file must be placed in empower/apps/sandbox/managers/flowmanager/descriptors/
-FLOW_DESCRIPTOR="experiment_run.json"       # 5 flows following POISSON distribution
-#FLOW_DESCRIPTOR="single_flow.json"         # 1 flow of 5Mbps following PERIODIC distribution (testing purposes)
-#FLOW_DESCRIPTOR="two_flows.json"           # 2 flows of 8Mbps following PERIODIC distribution (testing purposes)
-
 # MCDA manager filename,
 # the file must be placed in empower/apps/sandbox/managers/mcdamanager/descriptors/
 MCDA_DESCRIPTOR="mcdainput.json"  # RSSI, Channel load, AP load, AP expected load, and Queue delay with different weights
@@ -45,8 +39,11 @@ MCDA_DESCRIPTOR="mcdainput.json"  # RSSI, Channel load, AP load, AP expected loa
 #./empower-runtime.py apps.handlers.wifistatshandler --tenant_id=$TENANT_ID --db_monitor=True
 
 # Stats only
-./empower-runtime.py apps.handlers.lvapstatshandler --tenant_id=$TENANT_ID --db_monitor=True apps.handlers.wifistatshandler --tenant_id=$TENANT_ID --db_monitor=True apps.handlers.ncqmstatshandler --tenant_id=$TENANT_ID --db_monitor=True apps.handlers.ucqmstatshandler --tenant_id=$TENANT_ID --db_monitor=True apps.handlers.binstatshandler --db_monitor=True --tenant_id=$TENANT_ID apps.handlers.slicestatshandler --tenant_id=$TENANT_ID --db_monitor=True
+#./empower-runtime.py apps.handlers.lvapstatshandler --tenant_id=$TENANT_ID --db_monitor=True apps.handlers.wifistatshandler --tenant_id=$TENANT_ID --db_monitor=True apps.handlers.ncqmstatshandler --tenant_id=$TENANT_ID --db_monitor=True apps.handlers.ucqmstatshandler --tenant_id=$TENANT_ID --db_monitor=True apps.handlers.binstatshandler --tenant_id=$TENANT_ID --db_monitor=True apps.handlers.slicestatshandler --tenant_id=$TENANT_ID --db_monitor=True
+
+# WiFi Slicing
+#./empower-runtime.py apps.sandbox.managers.flowmanager.flowmanager --tenant_id=$TENANT_ID apps.sandbox.managers.wifislicemanager.wifislicemanager --tenant_id=$TENANT_ID --minimum_quantum=$MINIMUM_QUANTUM --quantum_decrease_rate=$QUANTUM_DECREASE_RATE --quantum_increase_rate=$QUANTUM_INCREASE_RATE apps.handlers.lvapstatshandler --tenant_id=$TENANT_ID --db_monitor=True apps.handlers.wifistatshandler --tenant_id=$TENANT_ID --db_monitor=True apps.handlers.ncqmstatshandler --tenant_id=$TENANT_ID --db_monitor=True apps.handlers.ucqmstatshandler --tenant_id=$TENANT_ID --db_monitor=True apps.handlers.binstatshandler --tenant_id=$TENANT_ID --db_monitor=True  apps.handlers.slicestatshandler --tenant_id=$TENANT_ID --db_monitor=True
 
 # Sandbox APPs
-#./empower-runtime.py apps.sandbox.managers.flowmanager.flowmanager --tenant_id=$TENANT_ID --descriptor=$FLOW_DESCRIPTOR apps.sandbox.managers.wifislicemanager.wifislicemanager --tenant_id=$TENANT_ID --minimum_quantum=$MINIMUM_QUANTUM --quantum_decrease_rate=$QUANTUM_DECREASE_RATE --quantum_increase_rate=$QUANTUM_INCREASE_RATE apps.sandbox.managers.mcdamanager.mcdamanager --tenant_id=$TENANT_ID --descriptor=$MCDA_DESCRIPTOR --db_monitor=True --db_user=$USER --db_pass=$PASS apps.handlers.lvapstatshandler --tenant_id=$TENANT_ID --db_monitor=True --db_user=$USER --db_pass=$PASS apps.handlers.wifistatshandler --tenant_id=$TENANT_ID --db_monitor=True --db_user=$USER --db_pass=$PASS apps.handlers.ncqmstatshandler --tenant_id=$TENANT_ID --db_monitor=True --db_user=$USER --db_pass=$PASS apps.handlers.ucqmstatshandler --tenant_id=$TENANT_ID --db_monitor=True --db_user=$USER --db_pass=$PASS apps.handlers.binstatshandler --db_monitor=True --db_user=$USER --db_pass=$PASS --tenant_id=$TENANT_ID apps.handlers.slicestatshandler --tenant_id=$TENANT_ID --db_monitor=True --db_user=$USER --db_pass=$PASS
+./empower-runtime.py apps.sandbox.managers.flowmanager.flowmanager --tenant_id=$TENANT_ID apps.sandbox.managers.wifislicemanager.wifislicemanager --tenant_id=$TENANT_ID --minimum_quantum=$MINIMUM_QUANTUM --quantum_decrease_rate=$QUANTUM_DECREASE_RATE --quantum_increase_rate=$QUANTUM_INCREASE_RATE apps.sandbox.managers.mcdamanager.mcdamanager --tenant_id=$TENANT_ID --descriptor=$MCDA_DESCRIPTOR --db_monitor=True apps.handlers.lvapstatshandler --tenant_id=$TENANT_ID --db_monitor=True apps.handlers.wifistatshandler --tenant_id=$TENANT_ID --db_monitor=True apps.handlers.ncqmstatshandler --tenant_id=$TENANT_ID --db_monitor=True apps.handlers.ucqmstatshandler --tenant_id=$TENANT_ID --db_monitor=True apps.handlers.binstatshandler --tenant_id=$TENANT_ID --db_monitor=True  apps.handlers.slicestatshandler --tenant_id=$TENANT_ID --db_monitor=True
 
