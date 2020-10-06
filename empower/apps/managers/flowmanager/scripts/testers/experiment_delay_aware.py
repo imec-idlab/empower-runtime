f1db6dc8208d2c8b33eb1c8e4b48be2de1bf6f15
phisolani <pedroisolani@gmail.com>
2020-10-06 21:12:37 +0200

    init script changes

--- empower/apps/managers/flowmanager/scripts/testers/experiment_delay_aware.py
+++ empower/apps/managers/flowmanager/scripts/testers/experiment_delay_aware.py
@@ -13,10 +13,8 @@ import time
 
 # Experimentation parameters and values
 parser = OptionParser()
-parser.add_option("", "--controller_ip", type="string", default="10.11.17.1")  # Empower controller (wilabt)
-#parser.add_option("", "--controller_ip", type="string", default="146.175.219.129")  # Empower controller (sandbox)
-parser.add_option("", "--tenant_id", type="string", default="f1160872-c9c6-4d7f-b7f7-3e4a01c62a90")  # wilabt
-#parser.add_option("", "--tenant_id", type="string", default="8aaca1c6-bf3c-4455-8c6d-4e4b6eef7719")  # sandbox
+parser.add_option("", "--controller_ip", type="string", default="143.129.76.37")  # Empower controller (sandbox)
+parser.add_option("", "--tenant_id", type="string", default="8aaca1c6-bf3c-4455-8c6d-4e4b6eef7719")  # sandbox
 parser.add_option("", "--user", type="string", default="root")  # e.g., root, user
 parser.add_option("", "--password", type="string", default="root")  # e.g., root, password
 
